a1b8ab66dfc97f7323c65b643c23f5be34fddbb9
Theo Pack Test <tf.pack@gmail.com>
2016-11-30 13:58:31 +0100

    Insert skills into redis

--- skill-stats/src/main/java/net.furikuri.skill/handler/SkillEventHandler.java
+++ skill-stats/src/main/java/net.furikuri.skill/handler/SkillEventHandler.java
@@ -8,21 +8,27 @@ import org.axonframework.eventhandling.annotation.EventHandler;
 import org.axonframework.eventhandling.replay.ReplayAware;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SkillEventHandler implements ReplayAware {
   private static final Logger LOG = LoggerFactory.getLogger(SkillEventHandler.class);
 
+  @Autowired
+  private RedisTemplate<String, String> redisClient;
+
   @EventHandler
   public void handle(SkillAddedEvent event) {
     LOG.info("Got event SkillAddedEvent");
+    redisClient.opsForValue().increment(event.getSkill(), 1);
   }
 
   @EventHandler
   public void handle(SkillDeletedEvent event) {
     LOG.info("Got event SkillDeletedEvent");
-
+    redisClient.opsForValue().increment(event.getSkill(), -1);
   }
 
   @Override
